f2365a2a3aaa925f86d724cbffdd440165312ae8
landonbuell <lhb1007@wildcats.unh.edu>
2020-08-20 23:53:08 -0400

    20 August 2020
    - Finished Program 4
    - Begun program 5

---
+++ MusicVisualizer/MusicVisualizer/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MusicVisualizer
+{
+
+    public class Program
+    {
+        public void MainProgram()
+        {
+            // Main program 
+        }
+    }
+
+
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MusicVisualizer/MusicVisualizer/MainPlayer.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Landon Buell
+ * Music Visulizer
+ * Main Player
+ * 19 August 2020
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Linq.Expressions;
+
+namespace MusicVisualizer
+{
+    class MainPlayer
+    {
+        public MainPlayer()
+        {
+            // Constructor for Main Player Object]
+            SoundPlayer Player = new SoundPlayer();
+            Player.
+
+        }
+
+    }
+}

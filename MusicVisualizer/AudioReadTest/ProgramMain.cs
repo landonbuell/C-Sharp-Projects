af8ec2886f587fc8ca1cc09da52539730f0666eb
landonbuell <lhb1007@wildcats.unh.edu>
2020-08-29 21:50:58 -0400

    29 August 2020
    - Updates to music visualizer

--- MusicVisualizer/AudioReadTest/ProgramMain.cs
+++ MusicVisualizer/AudioReadTest/ProgramMain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Media;
+using System.Threading;
 
 namespace AudioReadTest
 {
@@ -9,8 +10,19 @@ namespace AudioReadTest
         {
             string path = @"C:\Users\Landon\Music\CantinaBand60.wav";
 
-            SoundPlayer player = new SoundPlayer(path);
-            player.PlaySync();
+            SoundPlayer player1 = new SoundPlayer(path);
+            player1.Play();
+
+            Thread.Sleep(10000);
+
+            SoundPlayer player2 = new SoundPlayer(path);
+            player2.Play();
+
+            Thread.Sleep(10000);
+
+            
+
+            Console.WriteLine("=)");
         }
     }
 }
